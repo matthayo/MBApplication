416511038ff19890950370cd7b19af66761210df
Mathieu <mnyamagwa@gmail.com>
2017-11-08 22:31:48 -0600

    Added startup index file

--- Startup.cs
+++ Startup.cs
@@ -27,12 +27,23 @@ namespace MBApplication
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(
+            IApplicationBuilder app, 
+            IHostingEnvironment env,
+            ILoggerFactory loggerFactory)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+
+            app.UseDefaultFiles();
+
+            app.UseStaticFiles();
 
             app.UseMvc();
         }
