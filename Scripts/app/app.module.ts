ecfe47d5dfac3e14ba3cad14d3d47582a57de312
Mathieu <mnyamagwa@gmail.com>
2017-11-10 00:36:49 -0600

    Added AppModule - Initial commit

---
+++ Scripts/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
+import "rxjs/Rx";
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+    //directives, components and pipes
+    declarations: [
+        AppComponent
+    ],
+    
+    //Modules
+    imports: [
+        BrowserModule,
+        HttpModule
+     ],
+    
+     exports: [],
+    
+     // providers
+    providers: [],
+
+    bootstrap: [
+        AppComponent
+    ]
+})
+export class AppModule {}
