53256f540079e9c846a377b66ad240be1c998419
Mathieu O'r <mnyamagwa@gmail.com>
2017-11-21 11:50:04 -0600

    Changed AppCompoment template directive to templateUrl to call HMTL file from Scripts/html/app..

--- Scripts/app/app.component.ts
+++ Scripts/app/app.component.ts
@@ -1,10 +1,6 @@
 import { Component } from "@angular/core";
 @Component({
     selector: "start",
-    template: `<H1>MEMBERSHIP APPLICATION</H1>
-         <DIV>
-             WORK IN PROGRESS ....!
-         </DIV>
-         `
+    templateUrl: `../html/app.component.html`
 })
 export class AppComponent { }
