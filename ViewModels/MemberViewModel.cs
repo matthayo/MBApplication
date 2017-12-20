ca224e1a92e0becc021b0cf4131b3fbf3c5530d1
Mathieu <mnyamagwa@gmail.com>
2017-12-19 23:02:22 -0600

    MemberViewModel initial commit

---
+++ ViewModels/MemberViewModel.cs
@@ -0,0 +1,29 @@
+using System;
+using Newtonsoft.Json;
+
+namespace MBApplication.ViewModels
+{
+    [JsonObject(MemberSerialization.OptOut)]
+    public class MemberViewModel
+    {
+        //Constructor 
+        public MemberViewModel(){}
+
+        //Attributes
+        public int Id{get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; } 
+        public string LastName { get; set; }
+        public char Gender { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string MaritalStatus { get; set; }
+        public int MemberCount { get; set; }
+        public int OrgID { get; set; }
+        public int AddressID { get; set; }
+        public int MembershipID { get; set; }
+        public int FamilyID { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime LastModifiedDate { get; set; }
+  
+    }
+}
