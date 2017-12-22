4d09ba9553eae7197f5bb2c63198f297da82efcf
Mathieu <mnyamagwa@gmail.com>
2017-12-21 23:50:46 -0600

    PremiseViewModel class - Initial commit

---
+++ ViewModels/PremiseViewModel.cs
@@ -0,0 +1,22 @@
+using System;
+using Newtonsoft.Json;
+
+namespace MBApplication.ViewModels
+{
+    [JsonObject(MemberSerialization.OptOut)]
+    public class PremiseViewModel
+    {
+        // Constructor
+        public PremiseViewModel() {}
+
+        // Attributes
+        public int Id { get; set; }
+        public int MembershipId { get; set; }
+        public int AddressId { get; set; }
+        public string Type { get; set; }
+        [JsonIgnore]
+        public int Count { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime LastModifiedDate { get; set; }
+    }
+}
