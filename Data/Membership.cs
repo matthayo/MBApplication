836fd39a7cb84a593b046f814e2192c7b68b7cfc
Mathieu <mnyamagwa@gmail.com>
2017-12-21 23:47:59 -0600

    Membership model class - Initial commit

---
+++ Data/Membership.cs
@@ -0,0 +1,29 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MBApplication.Data
+{
+    public class Membership{
+
+        //Constructor
+        public Membership() {}
+
+        //Properties
+        [Required]
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int MemberId { get; set; }
+        [Required]
+        public int PremiseId { get; set; }
+        [Required]
+        public string Status { get; set; }
+        [Required]
+        public string MembershipBy { get; set; }
+        [Required]
+        public DateTime CreatedDate { get; set; }
+        [Required]
+        public DateTime LastModifiedDate { get; set; }
+    }
+    
+}
