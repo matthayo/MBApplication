4cd11110818676cd81b38a7e7fa760864516263d
Mathieu O'r <mnyamagwa@gmail.com>
2018-01-05 15:36:59 -0600

    Classes enhancement

--- Controllers/MembersController.cs
+++ Controllers/MembersController.cs
@@ -1,31 +1,110 @@
-using System.Collections.Generic;
+using AutoMapper;
 using MBApplication.ViewModels;
+using MBApplication.Data;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+using System;
 
 namespace MBApplication.Controllers
 {
     [Route("api/[controller]")]
     public class MembersController : Controller
     {
-        //GET api/members/GetNewMembers/10
-        [HttpGet("GetNewMembers/{num}")]
-        public JsonResult GetNewMembers(int num){
-            var member = new List<MemberViewModel>();
+        private readonly MBAppContext _dbContext;
+        private readonly IMapper _mapper;
+        
+        public MembersController(MBAppContext dbContext, IMapper mapper) 
+        {
+            _dbContext = dbContext; 
+            _mapper = mapper;   
+        }
 
-            for(int i = 1; i <= num; i++) 
-                member.Add(new MemberViewModel()
+        //GET api/members/Get/1
+        [HttpGet("Get/{id}")]
+        public IActionResult Get(int? id)
+        {
+            var member = _dbContext.Members.Where(i => i.Id == id).FirstOrDefault();    
+            if (id != null)
+            {
+                
+                return new JsonResult (_mapper.Map<MemberViewModel>(member), DefaultJsonSettings);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        //GET api/members/GetMembers
+        [HttpGet("GetMembers")]
+        public IActionResult GetMembers()
+        {
+            var members = _dbContext.Members.OrderBy(m => m.DateOfBirth).Take(AllMembers).ToList();
+
+            return new JsonResult(ToMemberViewModelList(members), DefaultJsonSettings);
+        }
+
+        //GET api/members/GetMembers/10
+        [HttpGet("GetMembers/{n}")]
+        public IActionResult GetMembers(int n)
+        {
+            var members = _dbContext.Members.OrderBy(m => m.DateOfBirth).Take(n).ToList();
+
+            return new JsonResult(ToMemberViewModelList(members), DefaultJsonSettings);
+        }
+
+        //GET api/members/GetFamilyMembers/1
+        [HttpGet("GetFamilyMembers/{id}")]
+        public IActionResult GetFamilyMembers(int? id)
+        {
+            var members = _dbContext.Members.Where(i => i.FamilyId == id);
+            if (id != null)
+            {
+
+                return new JsonResult(ToMemberViewModelList(members), DefaultJsonSettings);
+            }
+            else
+            {
+                return NotFound(new {Error = String.Format("Family ID {0} has not been found")});
+            }
+        }
+
+
+        //Return the total number of Members
+        public int  AllMembers
+        {
+             get
+             {
+                 return _dbContext.Members.Count();
+             }
+        }
+
+        // Maps a collection of Member entities into a list of MemberViewModel objects.
+        // Returns a mapped list of MemberViewModel objects.
+        private List<MemberViewModel> ToMemberViewModelList(IEnumerable<Member> members)
+        {
+            var membersList = new List<MemberViewModel>();
+
+            foreach (var member in members)
+            {
+                membersList.Add(_mapper.Map<MemberViewModel>(member));
+            }
+            return membersList;
+        }
+        
+        // Returns a suitable JsonSerializerSettings object that can be used to generate the 
+        // JsonResult return value for this Controller's methods.
+        private JsonSerializerSettings DefaultJsonSettings
+        {
+            get
+            {
+                return new JsonSerializerSettings()
                 {
-                    Id = i,
-                    FirstName = string.Format("Firstname{0}", i),
-                    LastName = string.Format("Lastname{0}", i)
-                });
-
-            var settings = new JsonSerializerSettings(){
-                Formatting = Formatting.Indented
-            };
-
-            return new JsonResult (member, settings);
+                    Formatting = Formatting.Indented
+                };
+            }
         }
     }
 }
