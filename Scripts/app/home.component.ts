f55ad153e0193788366090b0bfe99bbb0f219c1a
Mathieu O'r <mnyamagwa@gmail.com>
2018-01-10 15:20:38 -0600

    HomeComponent - Initial commit

---
+++ Scripts/app/home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "home",
+    templateUrl: `./html/home.component.html`
+})
+
+export class HomeComponent{
+    title = "Welcome View";
+}
