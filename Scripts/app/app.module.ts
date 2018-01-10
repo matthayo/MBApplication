c80aba424869f3cadf9d2cb18946a4d5ff53a1ac
Mathieu O'r <mnyamagwa@gmail.com>
2018-01-10 15:21:44 -0600

    Update AppModule with additions

--- Scripts/app/app.module.ts
+++ Scripts/app/app.module.ts
@@ -1,21 +1,28 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { HttpModule } from "@angular/http";
+import { RouterModule } from "@angular/router";
 import { Observable } from "rxjs/Rx";
 import "rxjs/Rx";
 
+import { AppRouting } from "./app.routing";
 import { AppComponent } from "./app.component";
+
+import { HomeComponent } from "./home.component";
 
 @NgModule({
     // directives, components and pipes
     declarations: [
-        AppComponent
+        AppComponent,
+        HomeComponent
     ],
 
     // modules
     imports: [
+        AppRouting,
         BrowserModule,
-        HttpModule
+        HttpModule,
+        RouterModule
      ],
 
      exports: [],
