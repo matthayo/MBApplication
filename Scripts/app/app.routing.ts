c005c9b2a59bd6d67c289a83b85212e6b0833089
Mathieu O'r <mnyamagwa@gmail.com>
2018-01-10 15:21:08 -0600

    AppRouting - Initial commit

---
+++ Scripts/app/app.routing.ts
@@ -0,0 +1,18 @@
+import { ModuleWithProviders } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+
+import { HomeComponent } from "./home.component";
+
+const appRoutes: Routes = [
+    {
+        path: "",
+        component: HomeComponent
+    },
+
+    {
+        path: "home",
+        redirectTo: ""
+    }
+];
+
+export const AppRouting: ModuleWithProviders = RouterModule.forRoot(appRoutes);
