78995826e18bfda9888ae30376da1bd9fb3e25de
Mathieu O'r <mnyamagwa@gmail.com>
2018-01-11 15:27:50 -0600

    NavbarComponent - Initial commit

---
+++ Scripts/app/navbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+import { expand } from "rxjs/operator/expand";
+
+@Component({
+    selector: "nav-bar",
+    templateUrl: "./html/navbar.component.html"
+})
+
+export class NavBarComponent{
+    
+}
