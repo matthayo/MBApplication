be44d626cca495c8fbf0237ec09c5328ed24b871
Mathieu O'r <mnyamagwa@gmail.com>
2018-01-26 16:11:47 -0600

    Added PageNotFoundComponent

---
+++ Scripts/app/misc/page-not-found.component.ts
@@ -0,0 +1,13 @@
+//Import
+import { Component } from "@angular/core";
+
+//Decorator
+@Component({
+    selector: "page-not-found",
+    templateUrl: "app/misc/page-not-found.component.html"
+})
+
+//Export
+export class PageNotFoundComponent {
+
+}
