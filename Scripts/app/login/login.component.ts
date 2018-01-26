7a36d3c19bf86bbefe173cbc67297c238f45b704
Mathieu O'r <mnyamagwa@gmail.com>
2018-01-26 16:11:12 -0600

    Added LoginComponent

---
+++ Scripts/app/login/login.component.ts
@@ -0,0 +1,14 @@
+//Import
+import { Component, Input } from "@angular/core";
+
+
+//Decorator
+@Component({
+    selector: "login",
+    templateUrl: "app/login/login.component.html"
+})
+
+//Export
+export class LoginComponent {
+
+}
