779530866cbde2689b15a16b4980dfddfec33a95
Mathieu O'r <mnyamagwa@gmail.com>
2018-01-31 16:54:35 -0600

    Added Bootstap css framework

--- Scripts/app/app.module.ts
+++ Scripts/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
 import { HttpModule } from "@angular/http";
 import { RouterModule } from "@angular/router";
+
 import { Observable } from "rxjs/Rx";
 import "rxjs/Rx";
 
--- gulpfile.js
+++ gulpfile.js
@@ -17,7 +17,9 @@ var srcPaths = {
         'node_modules/zone.js/dist/zone.js',
         'node_modules/reflect-metadata/Reflect.js',
         'node_modules/systemjs/dist/system.src.js',
-        'node_modules/typescript/lib/typescript.js'
+        'node_modules/typescript/lib/typescript.js',
+        'node_modules/ngx-bootstrap/bundles/ngx-bootstrap.umd.js',
+        'node_modules/moment/moment.js'
     ],
     js_angular: ['node_modules/@angular/**'],
     js_rxjs: ['node_modules/rxjs/**'],
