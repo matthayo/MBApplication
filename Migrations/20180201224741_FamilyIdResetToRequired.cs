f49f29a778f28a2f3822cab0f36e550e7c80a30e
Mathieu O'r <mnyamagwa@gmail.com>
2018-02-01 16:52:08 -0600

    Debug => with database tests

---
+++ Migrations/20180201224741_FamilyIdResetToRequired.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace MBApplication.Migrations
+{
+    public partial class FamilyIdResetToRequired : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
