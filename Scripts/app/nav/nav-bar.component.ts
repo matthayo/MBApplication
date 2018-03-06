73d5440eaad370c8718dd36473b2a6e42745a9c3
Mathieu <mnyamagwa@gmail.com>
2018-03-06 06:02:36 -0600

    Added Bootstrap CSS navbar features

---
+++ Scripts/app/nav/nav-bar.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit} from "@angular/core";
+import { expand } from "rxjs/operator/expand";
+
+@Component({
+    selector: "nav-bar",
+    templateUrl: "app/nav/nav-bar.component.html"
+})
+
+export class NavBarComponent{
+    logo: string;
+
+    constructor(){
+        this.logo = "assets/img/logo.png";
+    }
+
+    OnInit(){}
+}
