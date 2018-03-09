ca07a49479dac6af29567c4ef91d70ba78086ab1
Mathieu O'r <mnyamagwa@gmail.com>
2018-03-09 16:45:10 -0600

    Added footer component

---
+++ Scripts/app/footer/footer-section.component.ts
@@ -0,0 +1,14 @@
+//imports
+import { Component } from "@angular/core";
+import { expand } from "rxjs/operator/expand";
+
+//Decorators
+@Component({
+    selector: "footer-section",
+    templateUrl: "app/footer/footer-section.component.html"
+})
+
+//Export
+export class FooterSectionComponent{
+
+}
