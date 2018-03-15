d1fa37819102d30ce64946794dcf207fb053027c
Mathieu O'r <mnyamagwa@gmail.com>
2018-03-15 15:26:27 -0500

    Added new components.

---
+++ Scripts/app/login/logout.component.ts
@@ -0,0 +1,17 @@
+//Import
+import { Component } from "@angular/core";
+
+//Decorator
+@Component({
+    selector: "logout",
+    templateUrl: "app/login/logout.component.html"
+})
+
+//Export
+export class LogOutComponent{
+    title = "Login"
+
+    constructor(){
+
+    }
+}
