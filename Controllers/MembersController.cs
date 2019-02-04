516f0b75a55d37bcdcd84482990a91684f523961
Mathieu O'r <mnyamagwa@gmail.com>
2019-02-04 15:11:19 -0600

    Project Refactoring - MembersController.

--- Controllers/MembersController.cs
+++ Controllers/MembersController.cs
@@ -6,6 +6,7 @@ using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.Linq;
 using System;
+using Mapster;
 
 namespace MBApplication.Controllers
 {
@@ -13,12 +14,10 @@ namespace MBApplication.Controllers
     public class MembersController : Controller
     {
         private readonly ApplicationDbContext _dbContext;
-        private readonly IMapper _mapper;
         
-        public MembersController(ApplicationDbContext dbContext, IMapper mapper) 
+        public MembersController(ApplicationDbContext dbContext) 
         {
             _dbContext = dbContext;
-            _mapper = mapper;
         }
 
         //GET api/members/Get/1
@@ -28,7 +27,11 @@ namespace MBApplication.Controllers
             var member = _dbContext.Members.Where(i => i.Id == id).FirstOrDefault();    
             if (id != null && int.TryParse(id.ToString(), out var n))
             {
-                return new JsonResult (_mapper.Map<MemberViewModel>(member), DefaultJsonSettings);
+                return new JsonResult (
+                    member.Adapt<MemberViewModel>(),
+                    new JsonSerializerSettings(){
+                        Formatting = Formatting.Indented
+                    });
             }
             else
             {
@@ -40,18 +43,26 @@ namespace MBApplication.Controllers
         [HttpGet("GetAllMembers")]
         public IActionResult GetAllMembers()
         {
-            var members = _dbContext.Members.OrderBy(m => m.Id).Take(AllMembers).ToList();
+            var members = _dbContext.Members.OrderBy(m => m.Id).Take(AllMembers).ToArray();
 
-            return new JsonResult(ToMemberViewModelList(members), DefaultJsonSettings);
+            return new JsonResult(
+                members.Adapt<Member[]>(),
+                new JsonSerializerSettings(){
+                    Formatting = Formatting.Indented
+                });
         }
 
         //GET api/members/GetMembers/10
         [HttpGet("GetMembers/{n}")]
         public IActionResult GetMembers(int n)
         {
-            var members = _dbContext.Members.OrderBy(m => m.Id).Take(n).ToList();
+            var members = _dbContext.Members.OrderBy(m => m.Id).Take(n).ToArray();
 
-            return new JsonResult(ToMemberViewModelList(members), DefaultJsonSettings);
+            return new JsonResult(
+                members.Adapt<Member[]>(),
+                new JsonSerializerSettings(){
+                    Formatting = Formatting.Indented
+                });
         }
 
         //Get api/members/GetByLastName/{name}
@@ -60,7 +71,11 @@ namespace MBApplication.Controllers
             var member = _dbContext.Members.Where(i => i.LastName.Equals(name, StringComparison.OrdinalIgnoreCase));
             
             if(member != null){ 
-                return new JsonResult(ToMemberViewModelList(member), DefaultJsonSettings);
+                return new JsonResult(
+                    member.Adapt<Member>(),
+                    new JsonSerializerSettings(){
+                        Formatting = Formatting.Indented
+                    });
             }
             else
             {
@@ -73,10 +88,15 @@ namespace MBApplication.Controllers
         [HttpGet("GetFamilyMembersByName/{familyName}")]
         public IActionResult GetFamilyMembersByName(string familyName)
         {
-            var members = _dbContext.Members.Where(i => i.LastName.Equals(familyName, StringComparison.OrdinalIgnoreCase));
+            var members = _dbContext.Members.Where(i => i.LastName.Equals(familyName, StringComparison.OrdinalIgnoreCase)).ToArray();
             if (members != null)
             {
-                return new JsonResult(ToMemberViewModelList(members), DefaultJsonSettings);
+                return new JsonResult(
+                members.Adapt<Member[]>(),
+                new JsonSerializerSettings()
+                {
+                    Formatting = Formatting.Indented
+                });
             }
             else
             {
@@ -88,10 +108,15 @@ namespace MBApplication.Controllers
         [HttpGet("GetFamilyMembersById/{id}")]
         public IActionResult GetFamilyMembersById(int? id)
         {
-            var members = _dbContext.Members.Where(i => i.FamilyId == id);
+            var members = _dbContext.Members.Where(i => i.FamilyId == id).ToArray();
             if (id != null)
             {
-                return new JsonResult(ToMemberViewModelList(members), DefaultJsonSettings);
+                return new JsonResult(
+                 members.Adapt<Member[]>(),
+                 new JsonSerializerSettings()
+                 {
+                     Formatting = Formatting.Indented
+                 });
             }
             else
             {
@@ -99,73 +124,73 @@ namespace MBApplication.Controllers
             }
         }
 
-        //POST /api/members
-        [HttpPost()]
+        //PUT /api/members
+        [HttpPut]
         public IActionResult Add([FromBody]MemberViewModel memberToAdd)
         {
             if(memberToAdd != null)
-            {   
-                //Create a new Member from Json data
-                var member = _mapper.Map<Member>(memberToAdd);
-
-                //Override system-based variable
-                member.CreatedDate = DateTime.Now;
-                member.LastModifiedDate = DateTime.Now;
-
-                //Add the new member
-                _dbContext.Members.Add(member);
-
-                //Persist the changes
-                _dbContext.SaveChanges();
-
-                //Return the newly created member object
-                return new JsonResult(_mapper.Map<MemberViewModel>(member), DefaultJsonSettings);
-            }
-            else
             {
-                return new StatusCodeResult(500);
+                return new StatusCodeResult(500);   
             }
+            //Create a new Member from Json data
+            var member = memberToAdd.Adapt<Member>();
+
+            //Override system-based variable
+            member.CreatedDate = DateTime.Now;
+            member.LastModifiedDate = DateTime.Now;
+
+            //Add the new member
+            _dbContext.Members.Add(member);
+
+            //Persist the changes
+            _dbContext.SaveChanges();
+
+            //Return the newly created member object
+            return new JsonResult(
+                member.Adapt<MemberViewModel>(), 
+                new JsonSerializerSettings(){
+                    Formatting = Formatting.Indented
+                });
         }
 
-        //PUT api/members/1
-        [HttpPut("{id}")]
-        public IActionResult Update(int? id, [FromBody]MemberViewModel memberToUpdate)
+        //POST api/members
+        [HttpPost]
+        public IActionResult Post([FromBody]MemberViewModel memberToUpdate)
         {
-            if(id != null)
-            {
-                var member = _dbContext.Members.Where(i => i.Id == id).FirstOrDefault();
-
-                if(member != null)
-                {
-                    //Handling changes on property-basis
-                    member.FamilyId = memberToUpdate.FamilyId;
-                    member.FirstName = memberToUpdate.FirstName;
-                    member.MiddleName = memberToUpdate.MiddleName;
-                    member.LastName = memberToUpdate.LastName;
-                    member.Email = memberToUpdate.Email;
-                    member.Telephone = memberToUpdate.Telephone;
-                    member.Gender = memberToUpdate.Gender;
-                    member.MaritalStatus = memberToUpdate.MaritalStatus;
-                    member.DateOfBirth = memberToUpdate.DateOfBirth;
-
-                    //Overriding System-based variables
-                    member.LastModifiedDate = DateTime.Now;
-
-                    //persist changes to Database
-                    _dbContext.SaveChanges();
-
-                    //Return the newly update results
-                    return new JsonResult(_mapper.Map<MemberViewModel>(member), DefaultJsonSettings);
-                }
-                else
-                {
-                    return NotFound(new { Error = String.Format("Member ID {0} has not been found", id) });
-                }
-            }
-            else
+            if(memberToUpdate == null)
             {
                 return new StatusCodeResult(500);
             }
+
+            var member = _dbContext.Members.Where(i => i.Id == memberToUpdate.Id).FirstOrDefault();
+
+            if(member == null)
+            {
+                return NotFound(new { Error = String.Format("Member ID {0} has not been found", member.Id) });
+            }
+
+            //Handling changes on property-basis
+            member.FamilyId = memberToUpdate.FamilyId;
+            member.FirstName = memberToUpdate.FirstName;
+            member.MiddleName = memberToUpdate.MiddleName;
+            member.LastName = memberToUpdate.LastName;
+            member.Email = memberToUpdate.Email;
+            member.Telephone = memberToUpdate.Telephone;
+            member.Gender = memberToUpdate.Gender;
+            member.MaritalStatus = memberToUpdate.MaritalStatus;
+            member.DateOfBirth = memberToUpdate.DateOfBirth;
+
+            //Overriding System-based variables
+            member.LastModifiedDate = DateTime.Now;
+
+            //persist changes to Database
+            _dbContext.SaveChanges();
+
+            //Return the newly update results
+            return new JsonResult(member.Adapt<MemberViewModel>(),
+            new JsonSerializerSettings(){
+                Formatting = Formatting.Indented
+            });
         }
 
         //DELETE /api/delete/5
@@ -203,32 +228,6 @@ namespace MBApplication.Controllers
              {
                  return _dbContext.Members.Count();
              }
-        }
-
-        // Maps a collection of Member entities into a list of MemberViewModel objects.
-        // Returns a mapped list of MemberViewModel objects.
-        private List<MemberViewModel> ToMemberViewModelList(IEnumerable<Member> members)
-        {
-            var membersList = new List<MemberViewModel>();
-
-            foreach (var member in members)
-            {
-                membersList.Add(_mapper.Map<MemberViewModel>(member));
-            }
-            return membersList;
-        }
-        
-        // Returns a suitable JsonSerializerSettings object that can be used to generate the 
-        // JsonResult return value for this Controller's methods.
-        private JsonSerializerSettings DefaultJsonSettings
-        {
-            get
-            {
-                return new JsonSerializerSettings()
-                {
-                    Formatting = Formatting.Indented
-                };
-            }
         }
     }
 }
